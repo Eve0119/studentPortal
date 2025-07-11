354531cc1855a34e158af5c91a8b8759181aaaac
Eve0119 <gerictorilla@gmail.com>
2025-07-11 16:24:42 +0800

    Developing Edit Student Component

--- frontend/src/components/studentPage/EditStudent.jsx
+++ frontend/src/components/studentPage/EditStudent.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from 'react'
+import axiosInstance from '../../lib/axios';
 
-const EditStudent = () => {
-  return (
-    <div>EditStudent</div>
+const EditStudent = ({
+    isEditStudentOpen, 
+    setIsEditStudentOpen, 
+    studentId,
+    setStudent,
+    student
+}) => {
+    const editStudentRef = useRef(null);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        if (isEditStudentOpen && studentId) { 
+            editStudentRef.current?.showModal();
+            const fetchStudent = async () => {
+                setIsLoading(true);
+                setError(null);
+                try {
+                    const res =  await axiosInstance.get(`/student/${studentId}`);
+                    setStudent = res.data
+                } catch (error) {
+                    
+                }
+            }
+        }
+    },[])
+
+    return (
+    <div></div>
   )
 }
 
