8a2f348fb55bf2b211c6e56f857cbef8be55e802
Eve0119 <gerictorilla@gmail.com>
2025-07-17 14:03:20 +0800

    building and connecting Grade component to the App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,13 +5,14 @@ import Dashboard from './pages/Dashboard.jsx'
 import Navbar from './components/navigation/Navbar.jsx'
 import Sidebar from './components/navigation/Sidebar.jsx'
 import Students from './pages/Students.jsx'
+import Grades from './pages/Grades.jsx'
 
 const App = () => {
   const [sidebarOpen, setSidebarOpen] = useState(true);
 
   return (
     <div data-theme='lemonade' className="flex h-screen">
-      <Sidebar open={sidebarOpen} setOpen={setSidebarOpen} />
+      <Sidebar open={sidebarOpen} />
       <div className={`flex-1 flex flex-col ${sidebarOpen ? 'ml-30' : 'ml-0'}`}>
         <Navbar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
         <div className="flex-1 overflow-auto">
@@ -19,6 +20,7 @@ const App = () => {
             <Route path='/' element={<HomePage />} />
             <Route path='/dashboard/:id' element={<Dashboard />} />
             <Route path='/students' element={<Students />} />
+            <Route path='/grades' element={<Grades />} />
           </Routes>
         </div>
       </div>
---
+++ frontend/src/pages/Grades.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Grades = () => {
+  return (
+    <div>
+        <div className='bg-white flex w-auto border border-neutral-content p-4 m-7 rounded-lg'>
+            <div>
+                <h1 className='text-2xl font-bold'>Grades & Progress</h1>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Grades
