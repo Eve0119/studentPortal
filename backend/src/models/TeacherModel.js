fea3f0e1f3c63f366f33ff376f7b42cb899080fe
Eve0119 <gerictorilla@gmail.com>
2025-07-29 15:44:29 +0800

    teacher model

---
+++ backend/src/models/TeacherModel.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+
+const teacherSchema = new mongoose.Schema({
+    lastName: {
+        type: String, required: true
+    },
+    firstName: {
+        type: String, required: true
+    },
+    middleInitial: {
+        type: String, required: true
+    },
+    email: {
+        type: String, required: true, unique: true
+    },
+    assignedGradeLevel: {
+        type: String, required: true, enum: ['Pre-K', 'Kindergarten', 'Grade 1', 'Grade 2', 'Grade 3', 'Grade 4', 'Grade 5', 'Grade 6']
+    },
+    contactNumber: {type: String },
+    address: {
+        street: { type: String, required: true },
+        barangay: { type: String, required: true },
+        city: { type: String, required: true },
+        zip: { type: String, required: false }
+    },
+    dateOfBirth: {
+        type: Date, required: true
+    },
+    gender: {
+        type: String, enum: ['Male', 'Female'], required: true
+    },
+    dateHired: {
+        type: Date, required: true
+    },
+})
