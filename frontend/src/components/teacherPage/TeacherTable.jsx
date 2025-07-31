4732dc9db867884aa6a02cc9102a33a86316be2d
Eve0119 <gerictorilla@gmail.com>
2025-07-31 14:27:56 +0800

    created underdevelopment TeacherTable component

---
+++ frontend/src/components/teacherPage/TeacherTable.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const TeacherTable = () => {
+  return (
+    <div>     
+        <div className='flex flex-col w-auto m-7 p-6 gap-6 bg-white border border-neutral-content rounded-lg'>
+        <div>
+          <h1>Teaching Staff</h1>
+          <div>Manage and manage all teaching staff m</div>
+        </div>
+    </div></div>
+  )
+}
+
+export default TeacherTable
