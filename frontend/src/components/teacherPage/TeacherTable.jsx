ed17ecf40d3b71293781b5a4b9395d099acde0fa
Eve0119 <gerictorilla@gmail.com>
2025-08-05 10:39:43 +0800

    Teachers table design

--- frontend/src/components/teacherPage/TeacherTable.jsx
+++ frontend/src/components/teacherPage/TeacherTable.jsx
@@ -5,8 +5,8 @@ const TeacherTable = () => {
     <div>     
         <div className='flex flex-col w-auto m-7 p-6 gap-6 bg-white border border-neutral-content rounded-lg'>
         <div>
-          <h1>Teaching Staff</h1>
-          <div>Manage and manage all teaching staff m</div>
+          <h1 className='text-primary-content text-2xl font-bold md:text-2xl'>Teaching Staff</h1>
+          <div className='text-neutral-content text-sm md:text-base'>Manage and manage all teaching staff m</div>
         </div>
     </div></div>
   )
